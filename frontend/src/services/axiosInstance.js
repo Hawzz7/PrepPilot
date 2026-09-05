bf8625d1161cfa6a4efb7fa6ace7d4056ad41395
Narendra Kumar Majhi <vicky.007.cr@gmail.com>
2026-09-05 18:54:39 +0530

    commit

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const ServerURL =
-  import.meta.env.VITE_API_URL || "http://localhost:8000";
+const ServerURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const axiosInstance = axios.create({
   baseURL: ServerURL,
